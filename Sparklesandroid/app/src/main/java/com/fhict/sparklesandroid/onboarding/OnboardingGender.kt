93a0e84f5adc83021cbfe797c0d840121e2e75da
rickkorsten <rickkorsten@live.nl>
2018-11-07 18:46:36 +0100

    finished the code for onboard name and created onboard gender

---
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingGender.kt
@@ -0,0 +1,50 @@
+package com.fhict.sparklesandroid.onboarding
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.fhict.sparklesandroid.R
+import kotlinx.android.synthetic.main.activity_onboarding_gender.*
+
+class OnboardingGender : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_gender)
+
+        // get values
+        val name = intent.getStringExtra("name")
+        val maleBtn = findViewById<Button>(R.id.button_male)
+        val femaleBtn = findViewById<Button>(R.id.button_female)
+        val btn = findViewById<Button>(R.id.button)
+        var gender : String = "nothing"
+
+        maleBtn.setOnClickListener{
+            maleBtn.setBackgroundResource(R.drawable.onboard_gender_button_active)
+            femaleBtn.setBackgroundResource(R.drawable.onboard_gender_button_disabled)
+            btn.setBackgroundResource(R.drawable.onboard_button_green)
+            gender = "male"
+        }
+
+        femaleBtn.setOnClickListener{
+            femaleBtn.setBackgroundResource(R.drawable.onboard_gender_button_active)
+            maleBtn.setBackgroundResource(R.drawable.onboard_gender_button_disabled)
+            btn.setBackgroundResource(R.drawable.onboard_button_green)
+            gender = "female"
+        }
+
+        btn.setOnClickListener{
+            if(gender !== "nothing") {
+                startActivity(Intent(this, OnboardingGender::class.java)
+                        .apply {
+                            putExtra("name", name)
+                            putExtra("gender", gender)
+                        })
+            }else{
+                Toast.makeText(applicationContext, "$name  $gender",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
--- Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingName.kt
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingName.kt
@@ -1,7 +1,13 @@
 package com.fhict.sparklesandroid.onboarding
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.fhict.sparklesandroid.R
 
 class OnboardingName : AppCompatActivity() {
@@ -9,5 +15,43 @@ class OnboardingName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_name)
+        // get elements
+        var name = findViewById<EditText>(R.id.nameInput)
+        var button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            val inputtedText = name.text
+            if(inputtedText.isNotEmpty()) {
+                inputtedText.toString()
+                startActivity(Intent(this, OnboardingGender::class.java)
+                .apply {
+                                putExtra("name", inputtedText)
+            })
+            }else{
+                Toast.makeText(applicationContext,"please give us your name!",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        name.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                // Do something after text changed
+               // button.setBackgroundResource(R.drawable.onboard_button_green)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Do something before text changed on EditText
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Do something on text changed in EditText
+                // Display the EditText change text on TextView real time
+                if(p0?.length!! < 1){
+                    Toast.makeText(applicationContext,"nothing",Toast.LENGTH_SHORT).show()
+                    button.setBackgroundResource(R.drawable.onboard_button_grey)
+                }else{
+                    button.setBackgroundResource(R.drawable.onboard_button_green)
+                }
+            }
+        })
     }
 }
