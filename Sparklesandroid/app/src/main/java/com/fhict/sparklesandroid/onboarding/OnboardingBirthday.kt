b0e48564cf8c324709b14dbc992114f09f694dcc
Tom Gielen <tomgielen007@gmail.com>
2018-11-15 14:03:54 +0100

    Updated layouts, navigation and statusbar onboarding

--- Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingBirthday.kt
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingBirthday.kt
@@ -2,10 +2,12 @@ package com.fhict.sparklesandroid.onboarding
 
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -18,6 +20,12 @@ class OnboardingBirthday : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_birthday)
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.sparkle_button_grey))
+            window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.sparkle_background))
+        }
 
         // get values
         val extras = intent.extras ?: return
@@ -43,6 +51,9 @@ class OnboardingBirthday : AppCompatActivity() {
             }, year, month, day)
             // show dialog
             button.setBackgroundResource(R.drawable.onboard_button_green)
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setNavigationBarColor(getResources().getColor(R.color.sparkle_green))
+            }
             dpd.show()
         }
 
--- Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingWelcome.kt
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/onboarding/OnboardingWelcome.kt
@@ -1,8 +1,12 @@
 package com.fhict.sparklesandroid.onboarding
 
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.ColorInt
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import com.fhict.sparklesandroid.R
 
@@ -11,6 +15,12 @@ class OnboardingWelcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_welcome)
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.sparkle_green))
+            window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.sparkle_background))
+        }
 
         // get button
         val getStarted_btn = findViewById<Button>(R.id.button)
