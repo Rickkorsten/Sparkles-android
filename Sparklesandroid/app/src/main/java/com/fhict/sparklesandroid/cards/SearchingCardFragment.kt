61d589d40bab1ad2f7126283dda8ac7d6dd3be69
rickkorsten <rickkorsten@live.nl>
2018-11-21 14:17:41 +0100

    added classes to change the card on a given status

---
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/cards/SearchingCardFragment.kt
@@ -0,0 +1,20 @@
+package com.fhict.sparklesandroid.cards
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fhict.sparklesandroid.R
+
+class SearchingCardFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.searching_card, container, false)
+    }
+}
