aef7616ef4798634c713cec4e52dbecaa71e9593
Tom Gielen <tomgielen007@gmail.com>
2018-11-22 22:26:52 +0100

    Started styling chat, mainly implementing darkmode and custom header

--- Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/ChatActivity.kt
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/ChatActivity.kt
@@ -1,13 +1,41 @@
 package com.fhict.sparklesandroid
 
+import android.annotation.TargetApi
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.fhict.sparklesandroid.R
 
 class ChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setSparkTheme()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun setSparkTheme() {
+
+        val preferencesHelper = PreferencesHelper(applicationContext)
+
+        if (preferencesHelper.darkMode) {
+            setTheme(R.style.DarkTheme)
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.navigationBarColor = resources.getColor(R.color.black)
+                window.statusBarColor = resources.getColor(R.color.black)
+            }
+        } else {
+            setTheme(R.style.AppTheme_NoTitleBar)
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.navigationBarColor = resources.getColor(R.color.sparkle_background)
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                window.statusBarColor = resources.getColor(R.color.sparkle_background)
+            }
+        }
     }
 }
