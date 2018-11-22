fb90edf075cbefc679c21e8467e73887257a9b50
Tom Gielen <tomgielen007@gmail.com>
2018-11-22 17:39:14 +0100

    Added blurred image

--- Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/cards/MatchCardFragment.kt
+++ Sparklesandroid/app/src/main/java/com/fhict/sparklesandroid/cards/MatchCardFragment.kt
@@ -1,15 +1,23 @@
 package com.fhict.sparklesandroid.cards
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.request.RequestOptions
 import com.fhict.sparklesandroid.ChatActivity
+import com.fhict.sparklesandroid.GlideOptions.bitmapTransform
 import com.fhict.sparklesandroid.R
 import com.fhict.sparklesandroid.onboarding.OnboardingPreference
+import jp.wasabeef.glide.transformations.BlurTransformation
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 class MatchCardFragment : Fragment() {
 
@@ -21,6 +29,16 @@ class MatchCardFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view: View = inflater!!.inflate(R.layout.match_card, container, false)
+
+        val imageView = view.findViewById<ImageView>(R.id.iv_profilepic)
+
+        val multi = MultiTransformation<Bitmap>(
+                BlurTransformation(25),
+                RoundedCornersTransformation(128, 1))
+
+        Glide.with(this).load(R.drawable.bram)
+                .apply(bitmapTransform(multi))
+                .into(imageView)
 
        val toMessageButton = view.findViewById<LinearLayout>(R.id.messageClick)
 
